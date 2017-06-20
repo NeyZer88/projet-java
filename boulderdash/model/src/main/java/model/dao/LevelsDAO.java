bfc1786cdd1e1e54455e6ca4d4858fc18e9d582c
SCHITZCedric <cedric.schitz@viacesi.fr>
2017-06-20 16:09:13 +0200

    LevelDAO 3.0

--- boulderdash/model/src/main/java/model/dao/LevelsDAO.java
+++ boulderdash/model/src/main/java/model/dao/LevelsDAO.java
@@ -12,9 +12,8 @@ public abstract class LevelsDAO  extends AbstractDAO{
     private static String getLevel2   = "{call (Level2)}";
     private static String getLevel3   = "{call (Level3)}";
     private static String getLevel4   = "{call (Level4)}";
+    private static String getLevel5   = "{call (Level5)}";
     
-
-
 public static ArrayList<String> getLevel1(int line) throws SQLException {
     final ArrayList<String> elementsMap = new ArrayList<String>();
     final CallableStatement callStatement = prepareCall(getLevel1);
@@ -22,7 +21,6 @@ public static ArrayList<String> getLevel1(int line) throws SQLException {
     if (callStatement.execute()) {
         final ResultSet result = callStatement.getResultSet();
    
-		
 		for (int i = 0; i < 15; i++)
         {
         	elementsMap.add(result.getString(i));
@@ -32,7 +30,6 @@ public static ArrayList<String> getLevel1(int line) throws SQLException {
     return elementsMap;
 	}
 
-
 public static ArrayList<String> getLevel2(int line) throws SQLException {
     final ArrayList<String> elementsMap = new ArrayList<String>();
     final CallableStatement callStatement = prepareCall(getLevel2);
@@ -40,7 +37,6 @@ public static ArrayList<String> getLevel2(int line) throws SQLException {
     if (callStatement.execute()) {
         final ResultSet result = callStatement.getResultSet();
    
-		
 		for (int i = 0; i < 30; i++)
         {
         	elementsMap.add(result.getString(i));
@@ -49,7 +45,6 @@ public static ArrayList<String> getLevel2(int line) throws SQLException {
     }
     return elementsMap;
 	}
-
 
 public static ArrayList<String> getLevel3(int line) throws SQLException {
     final ArrayList<String> elementsMap = new ArrayList<String>();
@@ -58,7 +53,6 @@ public static ArrayList<String> getLevel3(int line) throws SQLException {
     if (callStatement.execute()) {
         final ResultSet result = callStatement.getResultSet();
    
-		
 		for (int i = 0; i < 30; i++)
         {
         	elementsMap.add(result.getString(i));
@@ -68,17 +62,13 @@ public static ArrayList<String> getLevel3(int line) throws SQLException {
     return elementsMap;
 	}
 
-
-
-
-public static ArrayList<String> getLevel5(int line) throws SQLException {
+public static ArrayList<String> getLevel4(int line) throws SQLException {
 	final ArrayList<String> elementsMap = new ArrayList<String>();
 	final CallableStatement callStatement = prepareCall(getLevel4);
 	callStatement.setInt(1, line);
 	if (callStatement.execute()) {
 		final ResultSet result = callStatement.getResultSet();
 
-	
 		for (int i = 0; i < 30; i++)
 		{
 			elementsMap.add(result.getString(i));
@@ -88,14 +78,13 @@ public static ArrayList<String> getLevel5(int line) throws SQLException {
 	return elementsMap;
 }
 	
-public static ArrayList<String> getLevel4(int line) throws SQLException {
+public static ArrayList<String> getLevel5(int line) throws SQLException {
 	final ArrayList<String> elementsMap = new ArrayList<String>();
-	final CallableStatement callStatement = prepareCall(getLevel4);
+	final CallableStatement callStatement = prepareCall(getLevel5);
 	callStatement.setInt(1, line);
 	if (callStatement.execute()) {
 		final ResultSet result = callStatement.getResultSet();
 
-	
 		for (int i = 0; i < 30; i++)
 		{
 			elementsMap.add(result.getString(i));
