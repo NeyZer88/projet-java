0c9c1ca863939f4d6ed601dcac5dfda5648517b3
SCHITZCedric <cedric.schitz@viacesi.fr>
2017-06-20 19:16:56 +0200

    Map

--- boulderdash/model/src/main/java/model/Map.java
+++ boulderdash/model/src/main/java/model/Map.java
@@ -2,4 +2,40 @@ package model;
 
 public class Map {
 
+	private int width;
+	private int height;
+	private int view;
+	private int quota;
+	
+	public int show(int yStart) {
+		return yStart;
+		
+	}
+	
+	public int getWidth() {
+		return width;
+	}
+	public void setWidth(int width) {
+		this.width = width;
+	}
+	public int getHeight() {
+		return height;
+	}
+	public void setHeight(int height) {
+		this.height = height;
+	}
+	public int getView() {
+		return view;
+	}
+	public void setView(int view) {
+		this.view = view;
+	}
+	public int getQuota() {
+		return quota;
+	}
+	public void setQuota(int quota) {
+		this.quota = quota;
+	}
+		
+
 }
