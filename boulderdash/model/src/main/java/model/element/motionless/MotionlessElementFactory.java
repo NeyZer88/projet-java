e392ab807f6353f858723fa3ca52b315eab1ba97
vinceExia <vincent.aubert@viacesi.fr>
2017-06-22 09:15:12 +0200

    MotionLessElementFactory 1.1

--- boulderdash/model/src/main/java/model/element/motionless/MotionlessElementFactory.java
+++ boulderdash/model/src/main/java/model/element/motionless/MotionlessElementFactory.java
@@ -1,5 +1,48 @@
 package model.element.motionless;
 
-public class MotionlessElementFactory {
+import model.element.Permeability;
 
+public abstract class MotionlessElementFactory {
+	
+	
+	
+	private static final Wallgame wallgame = new Wallgame();
+		
+	
+	
+	private static final Wallboard wallboard = new Wallboard();
+	
+	private static final Empty empty = new Empty();
+	
+	private static final Door door = new Door();
+	
+	private static MotionLessElement[] MotionLessElement = {wallgame, wallboard, empty, door, };
+	
+	
+	
+	public static MotionLessElement createWallboard(){
+		return wallboard;
+	}
+	
+	public static MotionLessElement createWallgame(){
+		return wallgame;
+	}
+	
+	public static MotionLessElement createEmpty(){
+		return empty;
+	}
+	
+	public static MotionLessElement createDoor(){
+		return door;
+	}
+	
+	public static MotionLessElement getFromFileSprite(String fileSprite){
+		for (final MotionLessElement motionlessElement: MotionLessElement){
+			if (motionlessElement.getSprite() == (fileSprite+".PNG")){
+				return motionlessElement;
+			}
+		}
+		
+		return empty; 
+	}
 }
