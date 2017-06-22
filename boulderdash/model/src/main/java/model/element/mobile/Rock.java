72710602a9e7e5124054c8ba83a6d2b434bd0535
vinceExia <vincent.aubert@viacesi.fr>
2017-06-22 14:52:37 +0200

    Elements 1.4

--- boulderdash/model/src/main/java/model/element/mobile/Rock.java
+++ boulderdash/model/src/main/java/model/element/mobile/Rock.java
@@ -6,7 +6,7 @@ import model.element.Permeability;
 import model.element.Sprite;
 
 public class Rock extends MobileElement {
-	
+
 	Rock(int x, int y) throws IOException{
 		super(x, y, SPRITE, Permeability.BLOCKING);
 	}
