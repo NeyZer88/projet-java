c4e4d1a89d130aa96b4abcf449568130f5cd7f6e
vinceExia <vincent.aubert@viacesi.fr>
2017-06-22 09:24:09 +0200

    Element 1.3

--- boulderdash/model/src/main/java/model/element/Element.java
+++ boulderdash/model/src/main/java/model/element/Element.java
@@ -1,24 +1,57 @@
 package model.element;
+import java.awt.image.*;
+import model.element.Permeability;
 
-public class Element {
+public abstract class Element{
 
-	public int x;
-	public int y;
+	public static int x;
+	public static int y;
 	public int HEIGHT = 16;
 	public int WIDTH  = 16;
 	public int immortal;
-	
-	public void getSprite(){
-	}
+	protected Permeability permeability;
+	protected static String sprite;
 
 	public void setSprite(){
 	}
 	
-	public boolean destructible(){
+	public final String getSprite(){
+		return this.sprite;
+	}
+
+	public final Permeability getPermeability(){
+		return this.permeability;
+	}
+	
+	private void setPermeability(final Permeability permeability){
+		this.permeability = permeability;
+	}
+	
+	protected boolean destructible(){
 	return false;
 	}
 	
 	public boolean superpose(){
 	return false;
+	}
+
+	public void goUp() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void goDown() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void goRight() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void goLeft() {
+		// TODO Auto-generated method stub
+		
 	}
 }
