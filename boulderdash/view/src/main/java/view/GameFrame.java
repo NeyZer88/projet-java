65eec70343085c2d7bd8b625d921fe9e4c71e479
blaudez <valentin.blaudez@viacesi.fr>
2017-06-22 13:52:24 +0200

    view 2.2

--- boulderdash/view/src/main/java/view/GameFrame.java
+++ boulderdash/view/src/main/java/view/GameFrame.java
@@ -1,57 +1,39 @@
 package view;
 
-import java.awt.BorderLayout;
-import java.awt.Dimension;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
-import javax.swing.JFrame;
-import javax.swing.JLabel;
 
-
-public class GameFrame implements KeyListener{
+public class GameFrame extends Jframe implements KeyListener{
+	private static final long serialVersionUID = -5222658361778310082L;
+	protected IEventPerformer eventPerformer;
+	private int keyCode;
 	
-	
-
-	public void GameFrame(){
-		this.setTitle ("boulderdash");
+	public IEventPerformer getIEventPerformer(){
+		return eventPerformer;
 	}
 	
-	
-	public class TestKeyListener extends JFrame {
-	    private static final long serialVersionUID = -5222658361778310082L;
-	 
-	    private TestKeyListener() {
-	        // création de la fenêtre
-	        super("TestKeyListener");
-	         
-	        // ajout d'un seul composant dans cette fenêtre : un JLabel
-	        JLabel label = new JLabel("tape pour test");
-	        add(label, BorderLayout.CENTER);
-	         
-	     // ajoute un écouteur d'événements personnalisé à la fenêtre
-	        addKeyListener(new TitreKeyListener(label));
-	        
-	
+	public void setIEventPerformer (IEventPerformer eventPerformer){
+		this.eventPerformer = eventPerformer;
+		eventPerformer.setGameFrame(this);
+	}
 
 }
 
 
 
-	public void keyPressed(KeyEvent arg0) {
-		// TODO Auto-generated method stub
+	public void keyPressed(KeyEvent e) {
+		keyCode = e.getKeyCode();
 		
 	}
 
-	public void keyReleased(KeyEvent arg0) {
-		// TODO Auto-generated method stub
+	public void keyReleased(KeyEvent e) {
+		keyCode = 0;
 		
 	}
 
 
-	public void keyTyped(KeyEvent arg0) {
-		// TODO Auto-generated method stub
-	
+	public void keyTyped(KeyEvent e) {	
 
 	}
 
--- boulderdash/view/src/main/java/view/ViewFacade.java
+++ boulderdash/view/src/main/java/view/ViewFacade.java
@@ -6,28 +6,43 @@ import javax.swing.JOptionPane;
  * <h1>The Class ViewFacade provides a facade of the View component.</h1>
 
  */
-public class ViewFacade implements IView {
+public class ViewFacade implements IViewSystem {
+	
+	
+	protected GraphicsBuilder graphicsBuilders;
+	protected GameFrame gameFrame;
+	
+	public ViewFacade(){
+		this.graphicsBuilders = new GraphicsBuilder();
+		this.gameFrame = new GameFrame();
+		this.graphicsBuilders.setViewFacade(this);
+		this.gameFrame.setGameFrame(this);
+	}
+	
+	public void displayMaps (int x, int y){
+		
+		
+	}
 
     /**
      * Instantiates a new view facade.
      */
 	
-    public ViewFacade() {
-        super();
-    }
 
-    /*
-     * @see view.IView#displayMessage(java.lang.String)
-     */
-    public final void displayMessage(final String message) {
-        JOptionPane.showMessageDialog(this, "message\n attention", "boulderdash", JOptionPane.WARNING_MESSAGE);
-    }
-    
-    public void displayMaps(String map){
-    	
-    }
+	@Override
+	public String displayMessage(String message) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
-	public void displayMap() {
+	@Override
+	public String displayMap(String message) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void closeAll() {
 		// TODO Auto-generated method stub
 		
 	};
