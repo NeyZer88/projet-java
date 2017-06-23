c233fb3fb9537d492341b701436494dc437d1402
NeyZer88 <nicolas.jerome1@viacesi.fr>
2017-06-23 11:27:59 +0200

    UserOrder Definitive edition

--- boulderdash/controller/src/main/java/controller/UserOrder.java
+++ boulderdash/controller/src/main/java/controller/UserOrder.java
@@ -1,15 +1,43 @@
 package controller;
 
+import model.element.mobile.Character;
+
 public class UserOrder implements IUserOrder {
 		
 //player move
-	
+		
+	//Set the player move
 	public  UserOrder(int player , Order order) {
 		
+		switch(order)
+		{
+		
+		case UP:
+			
+			Character.getUP();
+			
+
+		case DOWN :
+			
+			Character.getDOWN();
+
+			
+		case LEFT :
+			
+			Character.getLEFT();
+
+			
+		case RIGHT :
+
+			Character.getRIGHT();
+
+		
+		}
 	
 		
 	}
 	
+	// Get the player movement
 
 	public Order getOrder(Order order){
 		
@@ -18,6 +46,8 @@ public class UserOrder implements IUserOrder {
 		
 	}
 	
+	//Get the player 
+		
 	public char getPlayer(char player) {
 		
 		return player;
